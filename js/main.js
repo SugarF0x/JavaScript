539b7125f73da4d9fa905dfd4c80561e5b1cf905
SugarF0x <malion.torchwood@yandex.ru>
2019-03-20 19:52:30 +0300

    Task 1 finished Well, kinda finished I did do everything as instructed, but testing proved me wrong For some reason, it still assigns the very same object instead of making a copy

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
 "use strict";
 
+// 1
+console.log("    > Task 1 <     ");
+
+let a = [
+    [{name: 'Mary', age: 23}, {name: 'Mike', age: 45}, {name: 'Nick', age: 11}],
+    [{name: 'Adam', age: 56}, {name: 'Sara', age: 21}, {name: 'Don', age: 22}],
+    [{name: 'Karl', age: 34}, {name: 'Marta', age: 76}, {name: 'John', age: 19}]
+];
+
+let b = Object.assign({}, a);
+//a[0][0].name = "Saladass";    // dunno whats up, but for me <b> value also updates and console displays Saladass instead of Mary, tho i did as instructed :/
+console.log(b);
+
+// 2
+console.log("    > Task 2 <     ");
+
