3de9ff1d5f7f010d0f0f47ffe07faffdc533a55b
SugarF0x <malion.torchwood@yandex.ru>
2019-03-24 16:08:39 +0300

    Finished Task 1 It was a pain, i was trying to put everything in return object right away without creating unnecessary variables, but failed miserably In the end I didn't quite get how do I reference the variables within the very same object during a single initialisation (im not even sure if it's possible at all) so there you got, a peg I came up with Aint top efficient, but hey, as long as it woks (˵¯͒⌄¯͒˵)
    P.S. I just realised that this could have been done even easier - I could have first checked for the number to be less than 999, then convert it to String and then just extract 0, 1 and 2 char position (❁°͈▵°͈)

--- js/lessons/4.js
+++ js/lessons/4.js
@@ -1,1 +1,42 @@
 "use strict";
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function div(val, by){
+    return (val - val % by) / by;
+}
+
+// 1
+console.log("\n > Task 1 < \n");
+
+function getDecs(number) {
+    if (number > 999) {
+        console.log('The number is greater than 999 (' + number + ')\n' +
+            'Returning empty object');
+        return {};
+    }
+
+    let
+        hundreds =   div(number,100),
+        tens     =   div(number - hundreds*100,10),
+        singles  =      number - hundreds*100 - tens*10;
+
+
+    return {
+        hundreds: hundreds,
+        tens: tens,
+        singles: singles
+    }
+}
+
+console.log(getDecs(getRandomInt(0,1280)));
+
+// 2
+console.log("\n > Task 2 < \n");
+
+
+
+// 3
+console.log("\n > Task 3 < \n");
