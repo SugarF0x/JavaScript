ed6b568dc16bc9aab517bd2bd5cb5af718708693
SugarF0x <malion.torchwood@yandex.ru>
2019-03-24 18:06:41 +0300

    Finished Task 2 Finally, god, it was terrible Don't get me wrong, I enjoyed every second of it, nothing pushed me to work harder as much as bugs in my code do There were a couple of bugs which I couldn't really track, so I've spent at least an hour to do that and to fix it all If only you had seem my face when I saw the code running as intended ( ͡° ͜ʖ ͡°) I also did it a bit different from what you've asked from us in the hw.pdf, but I guess it works just as fine with random items in the cart as if it did with <double even> numbers Talking about task 3, I didn't really want to do it cuz i remember myself making the very same thing back in the school days, but in Pascal. I wanted to send you the .exe of that game, but unfortunately, as it turns out, i hadn't saved it when reinstalling my windows this summer :c Anyway, there you go °˖✧◝(⁰▿⁰)◜✧˖°

--- js/lessons/4.js
+++ js/lessons/4.js
@@ -8,7 +8,7 @@ function div(val, by){
     return (val - val % by) / by;
 }
 
-// 1
+// 1 \\ ----------------------------------------------------------------------------------------------------------------
 console.log("\n > Task 1 < \n");
 
 function getDecs(number) {
@@ -33,10 +33,65 @@ function getDecs(number) {
 
 console.log(getDecs(getRandomInt(0,1280)));
 
-// 2
+// 2 \\ ----------------------------------------------------------------------------------------------------------------
 console.log("\n > Task 2 < \n");
 
+let goods = [
+    {name: 'Sub 2 PdP', price: 0},
+    {name: 'G-Fuel',    price: 69},
+    {name: 'Memes',     price: 250},
+    {name: 'Chair',     price: 399},
+    {name: 'News',      price: 780},
+    {name: 'TikTok Ads',price: 999},
+    {name: 'Legs',      price: 1280}
+];
 
+let cart = {
+    items: [],
 
-// 3
+    add(good) {
+        let isFound = false;
+
+        this.items.forEach(function (it) {
+            if (it.name === good.name) {
+                it.amount++;
+                isFound = true;
+            }
+        });
+
+        if (!isFound) {
+            this.items.push({name: good.name, price: good.price, amount: 1});
+        }
+    },
+
+    getPriceTotal() {
+        let price = 0;
+        this.items.forEach(function (it) {
+            price += it.price*it.amount;
+        });
+        return price;
+    },
+
+    getAmountTotal() {
+        let amount = 0;
+        this.items.forEach(function (it) {
+            amount += it.amount;
+        });
+        return amount;
+    }
+};
+
+//
+
+for (let i = 1; i <= getRandomInt(1,25); i++) {
+    cart.add(goods[getRandomInt(0,6)]);
+}
+
+//console.log(cart);
+console.log('Cart item amount: ' + cart.getAmountTotal());
+console.log('Cart price total: ' + cart.getPriceTotal());
+
+// 3 \\ ----------------------------------------------------------------------------------------------------------------
 console.log("\n > Task 3 < \n");
+
+console.log("Nope");
