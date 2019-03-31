7a91c4db5168486120f73c7cc958a94df0550554
SugarF0x <malion.torchwood@yandex.ru>
2019-03-31 14:40:56 +0300

    Finished Task 2 I really didn't  feel like using stylesheet for the table so I inserted all the styles directly into the html Yeah, it may be messy if one reads into the html, but hey, im happy enough, so that just about does it for me for now

--- js/lessons/5.js
+++ js/lessons/5.js
@@ -37,10 +37,12 @@ let chessBoard = {
             }
             board += '</tr>';
         }
-        return '<table style="border: 1px solid black;"><tbody>' + board + '</tbody></table>';
+        return '<table style="border: 1px solid black; border-collapse: collapse;"><tbody>' + board + '</tbody></table>';
     },
 
     getCharByDigit(digit) {
+        // well i could make it via an array disassemble so that it returns AA if digit is 27 or AB if its is 28 and etc, but nah
+
         switch(digit) {
             case 1:  return 'A';
             case 2:  return 'B';
@@ -72,9 +74,87 @@ let chessBoard = {
     }
 };
 
-chessBoard.render(8,8);
-//chessBoard.render(getRandomInt(1,26),getRandomInt(1,26));
+//chessBoard.render(8,8);
+chessBoard.render(getRandomInt(1,26),getRandomInt(1,26));
+console.log('This task is displayed in HTML');
 
 // Task 2 \\ -----------------------------------------------------------------------------------------------------------
 console.log(' > Task 2 < ');
 
+let goods = [
+    {name: 'Sub 2 PdP', price: 0},
+    {name: 'G-Fuel',    price: 69},
+    {name: 'Memes',     price: 250},
+    {name: 'Chair',     price: 399},
+    {name: 'News',      price: 780},
+    {name: 'TikTok Ads',price: 999},
+    {name: 'Legs',      price: 1280}
+];
+
+let cart = {
+    items: [],
+
+    add(good) {
+        let isFound = false;
+
+        this.items.forEach(function (it) {
+            if (it.name === good.name) {
+                it.amount++;
+                isFound = true;
+            }
+        });
+
+        if (!isFound) {
+            this.items.push({name: good.name, price: good.price, amount: 1});
+        }
+    },
+
+    getPriceTotal() {
+        let price = 0;
+        this.items.forEach(function (it) {
+            price += it.price*it.amount;
+        });
+        return price;
+    },
+
+    getAmountTotal() {
+        let amount = 0;
+        this.items.forEach(function (it) {
+            amount += it.amount;
+        });
+        return amount;
+    },
+
+    generateTable() {
+        let tdStyle = 'style="border: 1px solid black; padding: 3px;">';
+
+        if (this.items.length === 0) return "Корзина пуста";
+
+        let board = '';
+
+        for (let y = 0; y <= this.items.length-1; y++) {
+            board += '<tr>';
+                board += '<td ' + tdStyle + this.items[y].name + '</td>';
+                board += '<td ' + tdStyle + this.items[y].price + '</td>';
+                board += '<td ' + tdStyle + this.items[y].amount + '</td>';
+            board += '</tr>';
+        }
+        return '<table style="border: 2px solid black; border-collapse: collapse;"><tbody><tr><td ' + tdStyle + 'Товар</td><td ' + tdStyle + 'Стоимость</td><td ' + tdStyle + 'Количество</td></tr>' +
+            board +
+            '<tr><td ' + tdStyle + 'Всего:</td><td ' + tdStyle +  this.getPriceTotal() + '</td><td ' + tdStyle + this.getAmountTotal() + '</td></tr></tbody></table>';
+    },
+
+    render() {
+        let table = this.generateTable();
+        document.body.insertAdjacentHTML('beforeend', table);
+    }
+};
+
+// cart functions I copy-pasted from lesson 4 and added <generateTable> and <render> functions to it ₍₍ (ง ˙ω˙)ว ⁾⁾
+
+for (let i = 1; i <= getRandomInt(1,25); i++) {
+    cart.add(goods[getRandomInt(0,6)]);
+}
+cart.render();
+
+console.log('This task is displayed in HTML');
