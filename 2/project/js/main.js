5c3be9ff6968e7e60f176000621d38430684d216
SugarF0x <malion.torchwood@yandex.ru>
2019-05-29 22:50:24 +0300

    Finished HW1

--- 2/project/js/main.js
+++ 2/project/js/main.js
@@ -1,3 +1,5 @@
+const currency = '$';
+
 const products = [
     {id: 1, title: 'Notebook', price: 2000},
     {id: 2, title: 'Mouse', price: 30},
@@ -6,17 +8,22 @@ const products = [
     {id: 5, title: 'Chair', price: 165},
 ];
 
-const renderProduct = (title, price) => {
+const renderProduct = (title = '[название]', price = '[цена]') => {
     return `<div class="product-item">
                 <h3>${title}</h3>
-                <p>${price}</p>
-                <button class="buy-btn">Купить</button>
+                <img src="https://via.placeholder.com/200x100/888888/FFFFFF?text=${title}.jpeg" alt="placeholder"></img>
+                <p>${price} ${currency}</p>
+                <button class="btn-buy">Купить</button>
             </div>`
 };
 
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.title, item.price));
-    document.querySelector('.products').innerHTML = productsList;
+const renderPage = (list = [{id:0, title: 'Not Defined', price: 0}]) => {
+    document.querySelector('.products').innerHTML = list.map(item => renderProduct(item.title, item.price)).join('');
+        // запятые отображались так как при внесении в качестве аргумента к .innerHTML массива, вносятся все его элементы через запятую
+        // через метод .join() можно превратить веьс массив в один большой String без запятых
+
+        // также не было необходимости создавать отдельную константу для последующего внесения её значения в document.querySelector()
+        // можно функцию сразу передать селектору в качестве значения
 };
 
 renderPage(products);
