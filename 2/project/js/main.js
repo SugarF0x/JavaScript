00e9c4bd6aab2ddda8fcb3da6927aaa96eed3558
SugarF0x <malion.torchwood@yandex.ru>
2019-06-07 10:00:05 +0300

    Finished HW2

--- 2/project/js/main.js
+++ 2/project/js/main.js
@@ -8,22 +8,84 @@ const products = [
     {id: 5, title: 'Chair', price: 165},
 ];
 
-const renderProduct = (title = '[название]', price = '[цена]') => {
+const renderProduct = (item) => {
     return `<div class="product-item">
-                <h3>${title}</h3>
-                <img src="https://via.placeholder.com/200x100/888888/FFFFFF?text=${title}.jpeg" alt="placeholder"></img>
-                <p>${price} ${currency}</p>
-                <button class="btn-buy">Купить</button>
+                <h3>${item.title}</h3>
+                <img src="https://via.placeholder.com/200x100/888888/FFFFFF?text=${item.title}.jpeg" alt="placeholder">
+                <p>${item.price} ${currency}</p>
+                <div class="btn-buy">
+                    <button onclick="cart.remove(products[${item.id-1}])">-</button>
+                    <button onclick="cart.add(products[${item.id-1}])">+</button>
+                </div>
             </div>`
 };
 
 const renderPage = (list = [{id:0, title: 'Not Defined', price: 0}]) => {
-    document.querySelector('.products').innerHTML = list.map(item => renderProduct(item.title, item.price)).join('');
-        // запятые отображались так как при внесении в качестве аргумента к .innerHTML массива, вносятся все его элементы через запятую
-        // через метод .join() можно превратить веьс массив в один большой String без запятых
-
-        // также не было необходимости создавать отдельную константу для последующего внесения её значения в document.querySelector()
-        // можно функцию сразу передать селектору в качестве значения
+    document.querySelector('.products').innerHTML = list.map(item => renderProduct(item)).join('');
 };
 
+//--------------------------------------------------------------------------------------------------------------------\\
+
+class Cart {
+    // {id: 0, title:'name', price:0, total:0;}
+    constructor() {
+        this.items = [];
+    }
+
+    add(item) {
+        let isFound = false;
+
+        this.items.forEach(it => {
+            if (it.id === item.id) {
+                it.total++;
+                isFound = true;
+            }
+        });
+
+        if (!isFound) {
+            this.items.push(new Product({id: item.id, title: item.title, price: item.price, total: 1}));
+        }
+    }
+
+    remove(item, total=1) {
+        this.items.forEach(it => {
+            if (it.id === item.id && it.total - total >= 0) {
+                it.total -= total;
+            }
+        });
+    }
+
+    getItemsTotal() {
+        let sum = 0;
+        this.items.forEach(item => sum += item.total);
+        return sum;
+    }
+
+    getPriceTotal() {
+        let sum = 0;
+        this.items.forEach(item => sum += item.getPriceTotal());
+        return sum;
+    }
+}
+
+class Product {
+    constructor(properties) {
+        this.id    = properties.id;
+        this.title = properties.title;
+        this.price = properties.price;
+        this.total = properties.total;
+    }
+
+    getPriceTotal() {
+        return this.price * this.total;
+    }
+}
+
+//--------------------------------------------------------------------------------------------------------------------\\
+
 renderPage(products);
+let cart = new Cart();
+
+// Я не успел сделать визуальную часть для корзины, но консольные функции работают как должны
+// cart.getPriceTotal() и cart.getItemsTotal() выводят суммарную цену и суммарное количество
+// cart.add() и cart.remove() назначены на кнопки (+) и (-) на странице соответственно
