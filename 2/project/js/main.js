4b2a5b3491e573c2961792fac0b544fc7ef1a559
Sugar_F0x <malion.torchwood@yandex.ru>
2019-06-07 14:47:32 +0300

    Finished with promises and respective callbacks

--- 2/project/js/main.js
+++ 2/project/js/main.js
@@ -1,12 +1,5 @@
-const currency = '$';
-
-const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 30},
-    {id: 3, title: 'Keyboard', price: 55},
-    {id: 4, title: 'Gamepad', price: 65},
-    {id: 5, title: 'Chair', price: 165},
-];
+const CURRENCY = '&#8381;';
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
 
 //--------------------------------------------------------------------------------------------------------------------\\
 
@@ -16,36 +9,121 @@ function screenSwap() {
         switch(swap) {
             case 'Корзина':
                 document.getElementById('btn-swap').innerText = 'Каталог';
+                document.getElementById('current').innerText = 'Корзина';
+                Array.from(document.getElementsByClassName('products')).forEach(element => {element.innerHTML=''});
+                // TODO: cart ui render
                 break;
             case 'Каталог':
                 document.getElementById('btn-swap').innerText = 'Корзина';
+                document.getElementById('current').innerText = 'Каталог';
+                Array.from(document.getElementsByClassName('products')).forEach(element => {element.innerHTML=''});
+                catalog.renderItemElement(catalog.goods);
                 break;
         }
     });
 }
 
+// function makeGETRequest(url, callback) {
+//     let xhr;
+//
+//     if (window.XMLHttpRequest) {
+//         xhr = new XMLHttpRequest();
+//     } else if (window.ActiveXObject) {
+//         xhr = new ActiveXObject("Microsoft.XMLHTTP");
+//     }
+//
+//     xhr.onreadystatechange = () => {
+//         if (xhr.readyState === 4) {
+//             callback(xhr.responseText);
+//         }
+//     };
+//
+//     xhr.open('GET', url, true);
+//     xhr.send();
+// }
+
+const promiseGETRequest = (url) => {
+    return new Promise(resolve => {
+        let xhr;
+
+        if (window.XMLHttpRequest) {
+            xhr = new XMLHttpRequest();
+        } else if (window.ActiveXObject) {
+            xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState === 4) {
+                resolve(xhr.responseText);
+            }
+        };
+
+        xhr.open('GET', url, true);
+        xhr.send();
+    });
+};
+
 //--------------------------------------------------------------------------------------------------------------------\\
 
 class Catalog {
-    constructor(list) {
-        this.renderItemElement(list);
+    constructor (list) {
+        this.goods = [
+            //{id: 1, title: 'Notebook', price: 2000},
+            //{id: 2, title: 'Mouse', price: 30},
+            {id: 3, title: 'Клавиатура', price: 2150},
+            {id: 4, title: 'Геймпад', price: 6500},
+            {id: 5, title: 'Стул', price: 1650},
+        ];
+        this.renderItemElement();
+        this.fetchGoods();
     }
 
     static _generateItemElement(item) {
         return `<div class="product-item">
-                    <h3>${item.title}</h3>
+                    <div>
+                        <h3>${item.title}</h3>
+                        <small>#${item.id}</small>
+                    </div>
                     <img src="https://via.placeholder.com/200x100/888888/FFFFFF?text=${item.title}.jpeg" alt="placeholder">
-                    <p>${item.price} ${currency}</p>
+                    <p>${item.price} ${CURRENCY}</p>
                     <button class="btn-buy" data-id="${item.id}">Добавить</button>
                 </div>`;
     }
 
-    renderItemElement(list = [{id:0, title: 'Not Defined', price: 0}]) {
+    renderItemElement(list = this.goods) {
         document.querySelector('.products').innerHTML = list.map(item => Catalog._generateItemElement(item)).join('');
 
         let classes = document.getElementsByClassName('btn-buy');
         Array.from(classes).forEach(element => {
             element.addEventListener('click', () => cart.add(element.getAttribute('data-id')));
+        });
+    }
+
+    fetchGoods() {
+        // makeGETRequest(`${API_URL}/catalogData.json`, goods => {
+        //     JSON.parse(goods).map(item => {
+        //         item.title  = item.product_name;
+        //         item.id     = item.id_product;
+        //
+        //         delete item.product_name;
+        //         delete item.id_product;
+        //
+        //         this.goods.push(item);
+        //         this.renderItemElement();
+        //     });
+        // });
+
+        promiseGETRequest(`${API_URL}/catalogData.json`).then((goods) => {
+            JSON.parse(goods).map(item => {
+                item.title  = item.product_name;
+                item.id     = item.id_product;
+
+                delete item.product_name;
+                delete item.id_product;
+
+                this.goods.push(item);
+                this.renderItemElement();
+            });
         });
     }
 }
@@ -56,9 +134,23 @@ class Cart {
         this.items = [];
     }
 
+    // TODO: ui render
+
+    _getItemById(id) {
+        let item = null;
+
+        catalog.goods.forEach(prod => {
+            if (prod.id === id) {
+                item = prod;
+            }
+        });
+
+        return item;
+    }
+
     add(id) {
         let isFound = false;
-        let item = this.getItemById(parseInt(id));
+        let item = this._getItemById(parseInt(id));
 
         this.items.forEach(it => {
             if (it.id === item.id) {
@@ -73,23 +165,14 @@ class Cart {
     }
 
     remove(item, total=1) {
-        this.items.forEach(it => {
+        this.items.forEach((it, id) => {
             if (it.id === item.id && it.total - total >= 0) {
                 it.total -= total;
+                if (it.total === 0) {
+                    this.items.splice(id,1);
+                }
             }
         });
-    }
-
-    getItemById(id) {
-        let item = null;
-
-        products.forEach(prod => {
-            if (prod.id === id) {
-                item = prod;
-            }
-        });
-
-        return item;
     }
 
     getItemsTotal() {
@@ -122,7 +205,7 @@ class Product {
 
 window.onload = screenSwap();
 let cart    = new Cart();
-let catalog = new Catalog(products);
+let catalog = new Catalog();
 
 // Я не успел сделать визуальную часть для корзины, но консольные функции работают как должны
 // cart.getPriceTotal() и cart.getItemsTotal() выводят суммарную цену и суммарное количество
